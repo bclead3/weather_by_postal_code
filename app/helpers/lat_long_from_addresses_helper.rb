10004264260ae5cc4a676b80c46c3952c0214611
Ben Leadholm <bclead3@yahoo.com>
2021-12-20 16:47:26 -0600

    Added clearer formatting of timestamp from database

--- app/helpers/lat_long_from_addresses_helper.rb
+++ app/helpers/lat_long_from_addresses_helper.rb
@@ -1,11 +1,48 @@
 module LatLongFromAddressesHelper
-  def format_time(time_str)
-    ts = DateTime.parse(time_str)
-    ts.strftime("")
+  TIME_ZONE_HASH = ActiveSupport::TimeZone::MAPPING
+  TIME_FORMAT = '%b %-d %-I:%M:%S %P'.freeze
+  DATE_FORMAT = '%Y  %b %-d'.freeze
+  def format_time(time_str, station_time_zone = nil)
+    if station_time_zone.present?
+      new_time_zone = convert_from_station_to_rails_tz(station_time_zone)
+      Time.zone = new_time_zone
+      Time.zone.parse(time_str).strftime(TIME_FORMAT)
+    else
+      time_stamp = DateTime.parse(time_str)
+      time_stamp.strftime(TIME_FORMAT)
+    end
   end
 
   def format_month_date(time_str)
     ts = DateTime.parse(time_str)
-    ts.strftime('%m/%d')
+    ts.strftime(DATE_FORMAT)
+  end
+
+  #private
+  def format_time_zone(str_time_zone)
+    return '' if str_time_zone.blank?
+
+    arr = str_time_zone.split('/')
+    city = arr.pop
+    country = arr.join('/')
+    "#{format_other(country)}/#{format_city(city)}"
+  end
+  def format_city(str_city)
+    str_city.split('/')&.last&.split(' ')&.map(&:capitalize)&.join('_')
+  end
+
+  # "America/Argentina/Buenos_Aires"
+  def format_other(str_time_zone)
+    return '' if str_time_zone.blank?
+
+    arr_country = str_time_zone.split('/')
+    arr_country&.map(&:capitalize)&.join('/')
+  end
+
+  def convert_from_station_to_rails_tz(station_time_zone)
+    rails_time_zone_value = format_time_zone(station_time_zone)
+    hashlet = TIME_ZONE_HASH.select{ |x, y| y == rails_time_zone_value}
+
+    hashlet.keys.first
   end
 end
--- spec/helpers/lat_long_from_addresses_helper_spec.rb
+++ spec/helpers/lat_long_from_addresses_helper_spec.rb
@@ -6,9 +6,43 @@ require 'rails_helper'
 RSpec.describe LatLongFromAddressesHelper, type: :helper do
   let(:now) { DateTime.now }
 
+  describe 'format_time' do
+    it 'formats the time w/o time zone' do
+      expect(helper.format_time('2021-12-20T18:00:00+00:00')).to eq('Dec 20 6:00:00 pm')
+    end
+
+    it 'formats the time with time zone' do
+
+      expect(helper.format_time('2021-12-20T18:00:00+00:00', convert_from_station_to_rails_tz('america/chicago'))).to eq('Dec 20 6:00:00 pm')
+    end
+
+    it 'format_city for retrieving appropriate time zone' do
+      expect(helper.format_city('america/chicago')).to eq('Chicago')
+      expect(helper.format_city('america/los angeles')).to eq('Los_Angeles')
+      expect(helper.format_city('america/new york')).to eq('New_York')
+      expect(helper.format_city('america/denver')).to eq('Denver')
+    end
+  end
+
+  describe 'format_other' do
+    it 'formats argentina' do
+      expect(helper.format_other('america/argentina')).to eq('America/Argentina')
+    end
+  end
+
+  describe 'format_time_zone' do
+    it 'handles "america/argentina/buenos aires" to output "America/Argentina/Buenos_Aires"' do
+      expect(helper.format_time_zone("america/argentina/buenos aires")).to eq("America/Argentina/Buenos_Aires")
+      expect(helper.format_time_zone('america/new york')).to eq('America/New_York')
+      expect(helper.format_time_zone('america/chicago')).to eq('America/Chicago')
+      expect(helper.format_time_zone('america/denver')).to eq('America/Denver')
+      expect(helper.format_time_zone('america/los angeles')).to eq('America/Los_Angeles')
+    end
+  end
+
   describe 'format_month_date' do
     it 'parses a DateTime timestamp into just the month and date components' do
-      expect(helper.format_month_date(now.to_s)).to eq("#{now.month}/#{now.day}")
+      expect(helper.format_month_date(now.to_s)).to eq("#{now.year}  #{now.strftime('%b')} #{now.strftime('%-d')}")
     end
   end
 end
